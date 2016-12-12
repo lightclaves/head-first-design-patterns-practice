f8231ffe893d5e75193a890cbbb3ff68aa5dce8a
Yaping.Yang <yaping.yang@tianheng-uestc.com>
2016-12-12 21:19:22 +0800

    Submit daily

--- P18/src/duck.java
+++ P18/src/duck.java
@@ -1,6 +1,22 @@
 
-public class duck {
-	public static void main(String []args) {
-        System.out.println("Hello World");
-    }
+public abstract class Duck {
+	FlyBehavior flyBehavior;
+	QuackBehavior quackBehavior;
+	public Duck(){
+		
+	}
+	public abstract void display();
+	
+	public void performFly(){
+		flyBehavior.fly();
+	}
+	
+	public void performQuack(){
+		quackBehavior.quack();
+	}
+	
+	public void swim(){
+		System.out.println("All ducks float, even decoys!");
+	}
+
 }
